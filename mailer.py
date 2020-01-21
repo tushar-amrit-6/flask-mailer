1c273b62b3e6628a92ea2673cef4d07ea8672aee
tushar-amrit-6 <48964465+tushar-amrit-6@users.noreply.github.com>
2020-01-21 10:17:01 +0530

    Add files via upload

---
+++ mailer.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_mail import Mail, Message
+
+app =Flask(__name__)
+mail=Mail(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'tusharpubg02@gmail.com'
+app.config['MAIL_PASSWORD'] = '*********'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route("/")
+def index():
+   msg = Message('Hello', sender = 'tusharpubg02@gmail.com', recipients = ['tushar.amrit.6@gmail.com'])
+   msg.body = "A mail to implement flask_mail"
+   mail.send(msg)
+   return "Message Sent "
+
+if __name__ == '__main__':
+   app.run(debug = True)
