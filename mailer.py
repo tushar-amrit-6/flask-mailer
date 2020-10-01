466bbc271e0a695983cd1125d3fca49d686a1ef4
shubham000231 <48961698+shubham000231@users.noreply.github.com>
2020-10-01 14:55:03 +0530

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -2,23 +2,30 @@ from flask import Flask
 from flask_mail import Mail, Message
 import pandas as pd
 
-## not imortant
+## not important
 
 app =Flask(__name__)
 mail=Mail(app)
 
+#smtp server
 app.config['MAIL_SERVER']='smtp.gmail.com'
+#port number is 465
 app.config['MAIL_PORT'] = 465
+#email id from which mail is sent.
 app.config['MAIL_USERNAME'] = 'tusharpubg02@gmail.com'
+
+#password
 app.config['MAIL_PASSWORD'] = '*********'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
+#creating object
 mail = Mail(app)
 
 @app.route("/")
 def index():
    msg = Message('Hello', sender = 'tusharpubg02@gmail.com', recipients = ['tushar.amrit.6@gmail.com'])
    msg.body = "A mail to implement flask_mail"
+   #sending mail
    mail.send(msg)
    return "Message Sent "
 
